1250e1aadecb53238ca698c0920f88641fcc48ac
maste150hhu <mastremolten2@gmail.com>
2020-07-14 09:00:53 +0200

    add fundamental functionality

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express')
 const app = express()
 
 app.set('view-engine', 'ejs');
+app.use(express.static(__dirname + '/css'));
+
+let database = new Map();
+
+database.set("marco", "https://www.bestofcode.net");
+
+let data;
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {name: "Marco"});
+    res.render('index.ejs', {data: database.entries()});
+});
+
+app.get('/:route', (req, res) => {
+    let destination = database.get(req.params.route);
+    console.log(req.params.route);
+    if(destination)
+        res.redirect(destination);
 });
